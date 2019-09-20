a442af491b6591d1368ba1ada5a959a7b903af71
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-09-20 15:07:36 +0300

    add simple note with title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+class DisplayNotes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      note: {
+        title: "",
+        content: ""
+      },
+      notes: []
+    };
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleTitleChange(e) {
+    const { value } = e.target;
+    this.setState(({ note }) => ({
+      note: {
+        ...this.state.note,
+        title: value
+      }
+    }));
+  }
+  handleContentChange(e) {
+    const { value } = e.target;
+    this.setState(({ note }) => ({
+      note: {
+        ...this.state.note,
+        content: value
+      }
+    }));
+  }
+  handleSubmit() {
+    this.setState((note, notes) => ({
+      notes: this.state.notes.concat(this.state.note),
+      note: {
+        title: "",
+        content: ""
+      }
+    }));
+  }
+  render() {
+    const styles = {
+      container: {
+        display: "grid",
+        justifyItems: "center"
+      }
+    };
+    console.log(this.state.notes);
+    const notesRender = this.state.notes.map(item => {
+      return <li key={item}>{item.title}</li>;
+    });
+    return (
+      <div style={styles.container}>
+        <h1>My notepad</h1>
+        <input
+          onChange={this.handleTitleChange}
+          value={this.state.note.title}
+          placeholder="title"
+        />
+        <br />
+        <input
+          onChange={this.handleContentChange}
+          value={this.state.note.content}
+          placeholder="content"
+        />
+        <br />
+        <button onClick={this.handleSubmit}>submit</button>
+        <ul>{notesRender}</ul>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default DisplayNotes;
